f80ae6c1f5e0b0834dccafa546238f02dc0f6c86
Stean <stean.ngugi@student.moringaschool.com>
2025-06-27 20:40:36 +0300

    Finished commit

---
+++ Products/src/components/ErrorMessage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const ErrorMessage = ({ message }) => (
+    <div className="text-center p-10 bg-red-50 text-red-700 rounded-lg max-w-2xl mx-auto my-8">
+        <h3 className="font-bold text-lg">Oops! Something went wrong.</h3>
+        <p className="mt-2">{message}</p>
+    </div>
+);
+export default ErrorMessage;
