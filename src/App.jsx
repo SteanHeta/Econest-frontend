d37e81e1ed31dab3f8a3834ff21c64969fbc199a
SteanL <stean5973@gmail.com>
2025-06-28 12:05:56 +0300

    Home (#7)
    * react app
    
    * Homepage
    
    * profile page
    
    * context
    
    * auth
    
    ---------
    
    Co-authored-by: Brian Cheptiony <cheptiony6@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="App bg-gray-50/70 min-h-screen">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
